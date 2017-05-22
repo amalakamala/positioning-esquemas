5ccfc775530f52baffda05f10309ee58321560d6
amalakamala <amalakamala.a@gmail.com>
2017-05-22 14:40:16 -0400

    Ejercicio 1 :Esquemas en JS

---
+++ js/main.js
@@ -0,0 +1,49 @@
+var cuadroUno = document.getElementsByClassName("cuadrado-uno")[0];
+var cuadroDos = document.getElementsByClassName("cuadrado-dos")[0];
+
+cuadroUno.style.margin = "10px";
+cuadroDos.style.margin = "10px";
+
+var rojo = document.getElementsByClassName("rojo")[0];
+var morado = document.getElementsByClassName("morado")[0];
+var amarillo = document.getElementsByClassName("amarillo")[0];
+
+
+rojo.style.position = "relative";
+rojo.style.background = "#FB0016";
+rojo.style.width = "300px";
+rojo.style.height = "300px";
+
+morado.style.position = "absolute";
+morado.style.background = "#671BFB";
+morado.style.width = "150px";
+morado.style.height = "150px";
+morado.style.top = "75px";
+morado.style.left = "75px";
+
+amarillo.style.position = "absolute";
+amarillo.style.background = "#FBD711";
+amarillo.style.width = "75px";
+amarillo.style.height = "75px";
+
+
+var azul = document.getElementsByClassName("azul")[0];
+var negro = document.getElementsByClassName("negro")[0];
+var verde = document.getElementsByClassName("verde")[0];
+
+azul.style.position = "relative";
+azul.style.background = "#0544D9";
+azul.style.width = "300px";
+azul.style.height = "300px";
+
+verde.style.position = "absolute";
+verde.style.background = "#08C531";
+verde.style.width = "150px";
+verde.style.height = "150px";
+verde.style.top = "75px";
+verde.style.left = "75px";
+
+negro.style.position = "absolute";
+negro.style.background = "#000601";
+negro.style.width = "75px";
+negro.style.height = "75px";
